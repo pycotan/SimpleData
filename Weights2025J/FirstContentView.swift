40087a01b27adbfdf821b75b05e703745fc2465f
hlrk <skysearainbow@gmail.com>
2025-02-13 21:34:34 +0900

    タイトル画面（仮）作成

---
+++ Weights2025J/FirstContentView.swift
@@ -0,0 +1,8 @@
+//
+//  FirstContentView.swift
+//  Weights2025J
+//
+//  Created by HLRK on 2025/01/05.
+//
+
+import Foundation
---
+++ Weights2025J/StrokeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  StrokeBackground.swift
+//  Weights2025J
+//
+//  Created by HLRK on 2025/02/13.
+//
+
+import SwiftUI
+
+struct StrokeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrokeBackground()
+}
