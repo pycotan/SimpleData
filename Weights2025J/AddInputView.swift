028404a899eb030e68f6bc37ec2ce589bc2a0ea8
hlrk <skysearainbow@gmail.com>
2025-03-26 09:04:43 +0900

    タブ追加　入力画面追加

---
+++ Weights2025J/AddInputView.swift
@@ -0,0 +1,18 @@
+//
+//  AddInputView.swift
+//  Weights2025J
+//
+//  Created by HLRK on 2025/03/26.
+//
+
+import SwiftUI
+
+struct AddInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddInputView()
+}
---
+++ Weights2025J/TabBarContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarContentView.swift
+//  Weights2025J
+//
+//  Created by HLRK on 2025/03/26.
+//
+
+import SwiftUI
+
+struct TabBarContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarContentView()
+}
