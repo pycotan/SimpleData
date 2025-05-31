ab365061d9568fcb093b40d4ff21846f87549498
hlrk <skysearainbow@gmail.com>
2025-05-31 20:38:37 +0900

    体重入力のテキストフィールド追加

--- Weights2025J/AddInputView.swift
+++ Weights2025J/AddInputView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct AddInputView: View {
+    @State var message:String = "データなし"
+    @State var double: Double = 0.0
+    @FocusState var isFocused: Bool
+    
     var body: some View {
-        Button(action: {
-            print("tap buton")
-        }) {
-            Text("体重入力")
-                .frame(maxWidth: .infinity, minHeight: 60)
-        }
-        .accentColor(Color.white)
-        .background(Color.blue)
-        .cornerRadius(.infinity)
-        .padding(.horizontal, 30)
+        VStack {
+            Button(action: {
+                print("tap buton")
+                isFocused = false
+                message = String(double)
+            }){
+                Text("体重入力")
+                    .frame(maxWidth: .infinity, minHeight: 60)
+            }
+            .accentColor(Color.white)
+            .background(Color.blue)
+            .cornerRadius(.infinity)
+            .padding(.horizontal, 50)
+            TextField("", value: $double, format: .number)
+                                .textFieldStyle(.roundedBorder)
+                    // ここでキーボードを数字のみにしている
+                    .keyboardType(.decimalPad)
+                    .padding(20)
+                    .multilineTextAlignment(.center)
+                    .focused($isFocused)
+            VStack {
+                Label("１ヶ月の平均体重", systemImage: "figure.walk.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.gray)
+                Text( message )
+                    .frame(maxWidth: .infinity, minHeight: 60)
+            }.padding()
+        }.padding()
     }
 }
 
