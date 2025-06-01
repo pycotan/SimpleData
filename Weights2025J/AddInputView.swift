047bbacf53957480ba1c66410b4c4f59c59f341c
hlrk <skysearainbow@gmail.com>
2025-06-01 13:55:35 +0900

    体重入力画面に日付表示追加

--- Weights2025J/AddInputView.swift
+++ Weights2025J/AddInputView.swift
@@ -11,6 +11,14 @@ struct AddInputView: View {
     @State var message:String = "データなし"
     @State var double: Double = 0.0
     @FocusState var isFocused: Bool
+    @State var dateText = ""
+    @State var nowDate = Date()
+    private let dateFormatter = DateFormatter()
+    
+    init() {
+        dateFormatter.dateFormat = "YYYY/MM/dd(E)HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "ja_jp")
+    }
     
     var body: some View {
         VStack {
@@ -33,13 +41,19 @@ struct AddInputView: View {
                     .padding(20)
                     .multilineTextAlignment(.center)
                     .focused($isFocused)
-            VStack {
-                Label("１ヶ月の平均体重", systemImage: "figure.walk.circle.fill")
+            Text( dateText.isEmpty ? "\(dateFormatter.string(from: nowDate))" : dateText)
+                .frame(maxWidth: .infinity, minHeight: 60)
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                        self.nowDate = Date()
+                        dateText = "\(dateFormatter.string(from: nowDate))"
+                    }
+                }
+            Label("１ヶ月の平均体重", systemImage: "figure.walk.circle.fill")
                             .font(.largeTitle)
                             .foregroundColor(.gray)
-                Text( message )
+            Text( message )
                     .frame(maxWidth: .infinity, minHeight: 60)
-            }.padding()
         }.padding()
     }
 }
