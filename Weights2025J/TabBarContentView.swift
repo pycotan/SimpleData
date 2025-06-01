f6ec682d134d25eb3ef62f8204ac4b4ad20b878e
hlrk <skysearainbow@gmail.com>
2025-06-01 14:08:39 +0900

    ナビゲージョンバーのラベル変更

--- Weights2025J/TabBarContentView.swift
+++ Weights2025J/TabBarContentView.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct TabBarContentView: View {
     // タブの選択項目を保持する
-        @State var selection = 1
+    @State var selection = 1
     
     var body: some View {
         TabView(selection: $selection) {
 
                     AddInputView()   // Viewファイル①
                         .tabItem {
-                            Label("Page1", systemImage: "1.circle")
+                            Label("入力", systemImage: "1.circle")
                         }
                         .tag(1)
 
                     TableContentView()   // Viewファイル②
                         .tabItem {
-                            Label("Page2", systemImage: "2.circle")
+                            Label("一覧", systemImage: "2.circle")
                         }
                         .tag(2)
 
                     FirstContentView()  // Viewファイル③
                         .tabItem {
-                            Label("Page3", systemImage: "3.circle")
+                            Label("設定", systemImage: "3.circle")
                         }
                         .tag(3)
 
