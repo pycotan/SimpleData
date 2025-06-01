6089c5adb29915f4df8aaf66dd535c50e6251c34
hlrk <skysearainbow@gmail.com>
2025-06-01 17:55:24 +0900

    TableViewにスワイプで削除ボタン追加　日時のフォーマット変更

--- Weights2025J/TableContentView.swift
+++ Weights2025J/TableContentView.swift
@@ -11,16 +11,34 @@ import SwiftData
 struct TableContentView: View {
     @Query private var weightDataList: [WeightData]
     @Environment(\.modelContext) private var modelContext
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "M月d日 HH時mm分"
+        // 日本語フォーマットに設定
+        return formatter
+    }
     
     var body: some View {
         NavigationStack {
-            List{ ForEach(Array(weightDataList.enumerated()), id: \.offset) { index, weightData in
+            List{ ForEach(Array(weightDataList.enumerated()), id: \.offset ) { index, weightData in
                 HStack {
                     Text(String(weightData.weightNum))
                     Spacer()
-                    Text(weightData.weightDate, format: .dateTime.month(.wide).day().year())
+                    Text(weightData.weightDate, formatter: dateFormatter)
                 }
+                .swipeActions {
+                            Button(role: .destructive) {
+                                deleteItem(weightData)
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .tint(.red)
+                        }
             }}}
+    }
+    
+    func deleteItem(_ item: WeightData) {
+        modelContext.delete(item)
     }
 }
 
